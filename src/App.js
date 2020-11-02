6ecbd9434d5f510097ab4b4310322434be9a9f8f
Kim yerim <kimyerim1935@naver.com>
2020-11-03 00:02:10 +0900

    img 삽입

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Take01 from '../SVG/Take01.jpg';
+import Take01 from './SVG/Take01.jpg';
 
 function App() {
   return (
     <div className="App">
       <div className="slide">
-        <img src={Take01} alt="take01" / >
+        <img src={Take01} alt="Take01" / >
       </div>
     </div>
   );
