c0cee3c0b1c80d67bb0d6708007b905213e7ac0b
Kim yerim <kimyerim1935@naver.com>
2020-11-03 21:38:55 +0900

    div classname

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Take01 from './SVG/Take01.jpg';
-import Slider from './Slider.js';
+import Slider from './Slider';
 
 function App() {
   return (
     <div className="App">
       <div className="slide">
-        <img src={Take01} alt="Take01" / >
+        
           <Slider / >
       </div>
     </div>
--- src/Slider.js
+++ src/Slider.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './Slider.css';
 
 const Slider = () => {
     return(
-        <div>
+        <div className="container">
             Slider
+        
         </div>
     )
 }
