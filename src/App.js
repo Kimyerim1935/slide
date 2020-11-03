a45f24a4184dc0524bbd17efbf8e3baa625a2dc4
Kim yerim <kimyerim1935@naver.com>
2020-11-03 18:30:30 +0900

    슬라이더 JS파일 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Take01 from './SVG/Take01.jpg';
+import Slider from './Slider.js';
 
 function App() {
   return (
     <div className="App">
       <div className="slide">
         <img src={Take01} alt="Take01" / >
+          <Slider / >
       </div>
     </div>
   );
---
+++ src/Slider.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Slider = () => {
+    return(
+        <div>
+            Slider
+        </div>
+    )
+}
+
+export default Slider;
