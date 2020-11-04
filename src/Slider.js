2bcfb4002579e5a61448050e78e2db438bdefc40
Kim yerim <kimyerim1935@naver.com>
2020-11-04 23:52:05 +0900

    swiper comp

--- src/Slider.js
+++ src/Slider.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Slider.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
 
-const Slider = () => {
+function Slider() {
     return(
-        <div className="container">
-            Slider
-        
-        </div>
+        <Swiper
+        spaceBetween={50}
+        slidesPerView={9}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
     );
 }
 
