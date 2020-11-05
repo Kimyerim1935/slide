4ad595c93916de241125760f1388f9f78f8f37e8
Kim yerim <kimyerim1935@naver.com>
2020-11-05 19:53:22 +0900

    코드 수정

--- src/Slider.js
+++ src/Slider.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import './Slider.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.scss';
+
+
 
 function Slider() {
     return(
-        <Swiper
-        spaceBetween={50}
-        slidesPerView={9}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
+     
     );
 }
 
