d62e9da06cb23c9771fe199d8d3c8486679d3f7b
Kim yerim <kimyerim1935@naver.com>
2020-11-07 15:06:39 +0900

    부트스트랩 설치

--- src/ImgSlider.js
+++ src/ImgSlider.js
@@ -1,21 +1,19 @@
 import React, {Component} from 'react';
 import './Slider.css';
-import Slider from 'react-slick';
 import Take01 from './SVG/Take01.jpg';
-
-const photos = [
-  {
-    name: 'Photo 1',
-    img:{Take01}
-  },
-  
-]
+import Take02 from './SVG/Take02.jpg';
+import Take03 from './SVG/Take03.jpg';
+import Take04 from './SVG/Take04.jpg';
+import Take05 from './SVG/Take05.jpg';
+import SwiperCore, { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 class ImgSlider extends Component {
   render(){
+
     return(
       <div>
-        hello
+   wow
       </div>
      ); 
 }
