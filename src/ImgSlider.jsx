338a4fc73eb36c8ce2b294c622d93ab42a7f6582
Kim yerim <kimyerim1935@naver.com>
2020-11-08 20:02:18 +0900

    이미지 추가

--- src/ImgSlider.jsx
+++ src/ImgSlider.jsx
@@ -9,6 +9,9 @@ import Take03 from './SVG/Take03.jpg';
 import Take04 from './SVG/Take04.jpg';
 import Take05 from './SVG/Take05.jpg';
 import Take06 from './SVG/Take06.jpg';
+import Take07 from './SVG/Take07.jpg';
+import Take08 from './SVG/Take08.jpg';
+import Take09 from './SVG/Take09.jpg';
 
 const ImgSliderConfigs = {
     contaimerClass:"swiper-contaimer img-slider",
@@ -52,6 +55,7 @@ const ImgSlider =() =>{
                  <img src={Take03} alt="img03"/>
              </div>
        </div>
+
        <div className="img-slide">
             <div
              className="slide-image"
@@ -61,6 +65,7 @@ const ImgSlider =() =>{
                  <img src={Take04} alt="img04"/>
              </div>
        </div>
+
        <div className="img-slide">
             <div
              className="slide-image"
@@ -78,7 +83,37 @@ const ImgSlider =() =>{
              >
                  <img src={Take06} alt="img06"/>
              </div>
-       </div>
+         </div>
+         <div className="img-slide">
+            <div
+             className="slide-image"
+             data-swiper-parallax={parallaxAmount}
+             data-swiper-parallax-opacity={parallaxOpacity}
+             >
+                 <img src={Take07} alt="img07"/>
+             </div>
+         </div>
+
+         <div className="img-slide">
+            <div
+             className="slide-image"
+             data-swiper-parallax={parallaxAmount}
+             data-swiper-parallax-opacity={parallaxOpacity}
+             >
+                 <img src={Take08} alt="img08"/>
+             </div>
+         </div>
+
+         <div className="img-slide">
+            <div
+             className="slide-image"
+             data-swiper-parallax={parallaxAmount}
+             data-swiper-parallax-opacity={parallaxOpacity}
+             >
+                 <img src={Take09} alt="img09"/>
+             </div>
+         </div>
+         
     </Swiper>;
 };
 
