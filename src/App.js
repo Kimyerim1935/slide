84c7afddc3cae6de39f70871510786b949e2ba7a
Kim yerim <kimyerim1935@naver.com>
2020-11-22 21:04:12 +0900

    6글자 입력 코드

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import React from 'react';
 
-function App() {
-  return (
-          <div className="num">
-          <form>
-            <input type="text" placeholder="Your Name"/>
-            <br/>
-            <button>Proceed</button>
-          </form>
-    </div>
-    
-  );
+class App extends React.Component {
+  state ={
+    disabled:true
+  }
+  handleChange = (e) => {
+    if(e.target.value.length >=6) {
+      this.setState({
+        disabled: false
+      });
+    }
+  }
+  render () {
+    return(
+      <div className="App">
+      <form>
+        <input type="text" placeholder="Your Name" onChange={this.handleChange} />
+        <br/>
+        <button disabled={this.state.disabled} >Proceed</button>
+
+      </form>
+</div>
+
+    );
+  }
 }
 
 export default App;
